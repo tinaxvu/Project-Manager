cd876e8f0cc968a2d1456d9c02ec70924fd5efe6
Grant Zou <grantbtzou@gmail.com>
2024-10-06 13:53:14 -0400

    testing redeploy

--- users/views.py
+++ users/views.py
@@ -9,4 +9,4 @@ def user_profile(request):
     user = request.user
     user_email = user.email
     user_groups = user.groups.all()
-    return render(request, 'users/user_profile.html', {'email': user_email, 'groups': user_groups})
+    return render(request, 'users/user_profile.html', {'email': user_email, 'groups': user_groups}) 
