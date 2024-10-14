f29db5be8fd0e259403de333a95dfe5a940e2447
Jerry Gu <ncq9fn@virginia.edu>
2024-10-14 10:36:24 -0400

    typo

--- AcademicProjectTracker/settings.py
+++ AcademicProjectTracker/settings.py
@@ -151,4 +151,4 @@ from decouple import config
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = config('AWS_REGION', default='us-east-1')
+AWS_S3_REGION_NAME = config('AWS_REGION', default='us-east-2')
