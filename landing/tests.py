350dff8a53affc273627d656602bc791818a3c63
tinavu <tinanvu04@gmail.com>
2024-10-18 06:22:36 -0400

    made tests for redirecting at login and specific pages

--- landing/tests.py
+++ landing/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class LandingTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_redirect_to_team_handbook(self):
+        response = self.client.get(reverse('team_handbook'), follow=True)
+
+        self.assertEqual(response.status_code, 200)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User, Group
+from django.test.client import Client
 
-# Create your tests here.
+class UserTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('testuser@gmail.com', 'password')
+
+    def test_logged_in_user_profile_view(self):
+        self.client.login(email='testuser@gmail.com',password='password')
+        response = self.client.get(reverse('landing_page'))
+
+        self.assertEqual(response.status_code, 200)
+    
