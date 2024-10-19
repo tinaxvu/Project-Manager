04ba7138be46e3aba0f1b239d6da4376a9e42bff
tinavu <tinanvu04@gmail.com>
2024-10-19 04:34:51 -0400

    database tests

--- landing/tests.py
+++ landing/tests.py
@@ -1,10 +1,1 @@
 from django.test import TestCase, Client
-from django.urls import reverse
-
-# test was working when there existed a url path to team handbook
-
-# class LandingTests(TestCase):
-#     def test_redirect_to_team_handbook(self):
-#         response = self.client.get(reverse('team_handbook'), follow=True)
-
-#         self.assertEqual(response.status_code, 200)
