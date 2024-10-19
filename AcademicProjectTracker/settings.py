a2fb9d7e666a8fd10ea76c21e00068f14aab92a0
Grant Zou <grantbtzou@gmail.com>
2024-10-19 18:50:58 -0400

    idk

--- AcademicProjectTracker/settings.py
+++ AcademicProjectTracker/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 
+if 'test' not in sys.argv:
+    django_heroku.settings(locals())
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
