f9501d1191cce8fa4787767695e99d81afd5be38
samyukrishnasamy <frv9sc@virginia.edu>
2024-10-21 23:59:40 -0400

    added nav bar, changed request to join so it only uses a button instead of leading you to a new page.

--- landing/views.py
+++ landing/views.py
@@ -2,7 +2,6 @@
 from django.shortcuts import render
 from projects.models import Project
 
-
 def landing_page(request):
     if request.user.is_authenticated:
         # Projects created by the user
@@ -15,10 +14,16 @@ def landing_page(request):
         all_projects = Project.objects.exclude(created_by=request.user).exclude(
             id__in=joined_projects.values_list('id', flat=True))
 
-        return render(request, 'landing/index.html', {
+        # All projects the user requested
+        requested_projects = request.user.projects_requested_by_users.all()
+
+        context = {
             'user_projects': user_projects,
             'joined_projects': joined_projects,
-            'all_projects': all_projects
-        })
+            'all_projects': all_projects,
+            'requested_projects': requested_projects,
+        }
+
+        return render(request, 'landing/index.html', context)
     else:
         return render(request, 'landing/index.html')
---
+++ projects/context_processors.py
@@ -0,0 +1,12 @@
+# projects/context_processors.py
+from .models import Project
+
+def project_context(request):
+    if request.user.is_authenticated:
+        user_projects = Project.objects.filter(created_by=request.user)
+        joined_projects = request.user.member_projects.exclude(created_by=request.user)
+        return {
+            'user_projects': user_projects,
+            'joined_projects': joined_projects,
+        }
+    return {}
