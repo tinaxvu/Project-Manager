72b0000f75d3581d1d661f1d7456dbc858f1cc22
Youness Charfaoui <cru8jn@virginia.edu>
2024-11-01 21:41:49 -0400

    add project views with more  management features

--- projects/views.py
+++ projects/views.py
@@ -7,6 +7,9 @@ from .models import Project
 from .forms import ProjectForm, FileUploadForm
 from .utils import get_signed_url
 from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import *
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 @login_required
@@ -94,10 +97,26 @@ def deny_request(request, project_id, user_id):
     return redirect('projects:project-detail', project_id=project_id)
 
 
-@login_required
-def calendar_view(request):
-    return render(request, 'specific_pages/calendar.html')
+from django.contrib import messages
+from .models import Calendar
 
+@login_required
+def calendar_view(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        event_date = request.POST.get('event_date')
+        created_by = request.user
+        
+        if title and description and event_date:
+            event = Calendar(title=title, description=description, event_date=event_date, project=project, created_by=created_by)
+            event.save()
+            messages.success(request, 'Event added successfully!')
+        else:
+            messages.error(request, 'Please fill in all fields.')
+    events = Calendar.objects.filter(project=project)
+    return render(request, 'specific_pages/calendar.html', {'events': events, 'project_id': project_id})
 
 @login_required
 def team_handbook_view(request):
@@ -114,13 +133,56 @@ def collaboration_view(request, project_id):
 
 
 @login_required
-def todos_view(request):
-    return render(request, 'specific_pages/todos.html')
+def todos_view(request, project_id):
+    if request.method == 'POST':
+        # Process the form data
+        description = request.POST.get('description')
+        date_due = request.POST.get('date_due')
+        priority = request.POST.get('priority')
+        assigned_to = request.POST.getlist('assigned_to')
+        is_finished = request.POST.get('is_finished', 'off') == 'on'
+
+        # Assuming there's a project_id in the session or a similar way to determine the project
+        # project_id = request.session.get('current_project_id')
+        project = get_object_or_404(Project, id=project_id)
+
+        # Create a new Todo instance
+        todo = Todo(project=project, description=description, date_due=date_due, priority=priority, is_finished=is_finished, created_by=request.user)
+        todo.save()
+
+        # Assign users to the todo
+        for user_id in assigned_to:
+            user = get_object_or_404(User, id=user_id)
+            todo.assigned_to.add(user)
+        return redirect('/')
+
+    users = User.objects.all()
+    todos = Todo.objects.filter(project=project_id)
+    return render(request, 'specific_pages/todos.html', {'users': users, 'todos': todos, 'project_id': project_id})
+@login_required
+def todo_detail(request,todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    return render(request, 'specific_pages/todo_detail.html', {'todo': todo})
+
+@login_required
+def toggle_todo_complete(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    todo.is_finished = not todo.is_finished
+    todo.save()
+    return redirect('projects:todo-detail', todo_id=todo.id)
+
 
 
 @login_required
-def timeline_view(request):
-    return render(request, 'specific_pages/timeline.html')
+def timeline_view(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    if request.method == 'POST':
+        event = request.POST.get('event')
+        date = request.POST.get('date')
+        if event and date:
+            Timeline(project=project, event=event, date=date).save()
+    events = Timeline.objects.filter(project=project_id)
+    return render(request, 'specific_pages/timeline.html', {'events': events, 'project_id': project_id})
 
 
 @login_required
@@ -157,3 +219,48 @@ def delete_file(request, project_id, file_id):
         return redirect('projects:project_files', project_id=project_id)
     else:
         return redirect('projects:project-detail', project_id=project_id)
+    
+
+@login_required
+def delete_project(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+
+    project.delete()
+    return JsonResponse({'success': True})
+
+
+
+@login_required
+def delete_todo(request, project_id, todo_id):
+    project = get_object_or_404(Project, id=project_id)
+    todo_to_delete = get_object_or_404(project.todos.all(), id=todo_id)
+
+    
+    todo_to_delete.delete()
+    return redirect('projects:todos', project_id=project_id)
+    
+
+@login_required
+def delete_calendar(request, project_id, calendar_id):
+    
+    calendar_to_delete = get_object_or_404(Calendar, id=calendar_id)
+
+    calendar_to_delete.delete()
+    return redirect('projects:calendar', project_id=project_id)
+    
+
+
+@login_required
+def delete_event(request, project_id, event_id):
+    
+    event_to_delete = get_object_or_404(Timeline, id=event_id)
+
+    
+    event_to_delete.delete()
+    return redirect('projects:timeline', project_id=project_id)
+    
+
+
+        
+
+
