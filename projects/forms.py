ca9782554e2b9a164a815a96f49273ec394d8d63
Youness Charfaoui <cru8jn@virginia.edu>
2024-11-01 21:33:00 -0400

    Add styling to forms

--- projects/forms.py
+++ projects/forms.py
@@ -7,9 +7,16 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
 
 
 class FileUploadForm(forms.ModelForm):
     class Meta:
         model = FileUpload
         fields = ['file']
+        widgets = {
+            'file': forms.FileInput(attrs={'class': 'form-control'}),
+        }
