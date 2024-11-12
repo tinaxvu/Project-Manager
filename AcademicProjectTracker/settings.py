88d3c0d966738789a722668996a8b153b7e35b86
Jerry Gu <ncq9fn@virginia.edu>
2024-11-12 11:20:59 -0500

    hardcoding bucket name

--- AcademicProjectTracker/settings.py
+++ AcademicProjectTracker/settings.py
@@ -159,10 +159,10 @@ from decouple import config
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'b-19-cs3240'
 AWS_S3_REGION_NAME = config('AWS_REGION', default='us-east-2')
 
 # Storage settings
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = 'b-19-cs3240.s3.amazonaws.com'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
