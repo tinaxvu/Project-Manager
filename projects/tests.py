96e4ead8f1c488a1e19f8de3eee5e27ccc88164c
tinavu <tinanvu04@gmail.com>
2024-12-05 00:57:37 -0500

    more meaningful model tests

--- projects/tests.py
+++ projects/tests.py
@@ -21,7 +21,9 @@ class Models(TestCase):
         self.assertTrue(Project.objects.filter(id=self.project.id).exists())
         self.assertTrue(get_user_model().objects.filter(username='tester').exists())
 
-    def test_create_todo(self):
+    def test_todo(self):
+        self.assertFalse(Todo.objects.filter(description='Test todo').exists())
+
         todo = Todo.objects.create(
             project=self.project,
             description='Test todo',
@@ -29,22 +31,17 @@ class Models(TestCase):
             priority='High',
             created_by=self.user
         )
+        self.assertTrue(Todo.objects.filter(description='Test todo').exists())
         self.assertEqual(todo.description, 'Test todo')
         self.assertEqual(todo.date_due, '2028-12-01')
         self.assertFalse(todo.is_finished)
 
-    def test_delete_todo(self):
-        todo = Todo.objects.create(
-            project=self.project,
-            description='Test todo',
-            date_due='2028-12-01',
-            priority='High',
-            created_by=self.user
-        )
         todo.delete()
         self.assertEqual(Todo.objects.count(), 0)
 
-    def test_create_calendar_event(self):
+    def test_calendar_event(self):
+        self.assertFalse(Calendar.objects.filter(title='Test event').exists())
+
         event = Calendar.objects.create(
             project=self.project,
             title='Test event',
@@ -55,26 +52,17 @@ class Models(TestCase):
             type='meeting',
         )
         self.assertEqual(event.title, 'Test event')
-        self.assertEqual(event.description, 'Test description')
         self.assertEqual(event.event_date, '2028-12-01 00:00')
         self.assertEqual(event.end_date, '2028-12-01 23:59')
         self.assertEqual(event.project, self.project)
-        self.assertTrue(Calendar.objects.filter(project=self.project).exists())
+        self.assertTrue(Calendar.objects.filter(title='Test event').exists())
 
-    def test_delete_calendar_event(self):
-        event = Calendar.objects.create(
-            project=self.project,
-            title='Test event',
-            description='Test description',
-            event_date='2028-12-01 00:00',
-            end_date='2028-12-01 23:59',
-            created_by=self.user,
-            type='meeting',
-        )
         event.delete()
         self.assertEqual(Calendar.objects.count(), 0)
 
     def test_schedule_meets(self):
+        self.assertFalse(ScheduleMeet.objects.filter(title='Test meeting').exists())
+
         meeting = ScheduleMeet.objects.create(
             project=self.project,
             title='Test meeting',
@@ -84,20 +72,16 @@ class Models(TestCase):
             end_time='12:30',
         )
         self.assertEqual(meeting.title, 'Test meeting')
+        self.assertTrue(ScheduleMeet.objects.filter(title='Test meeting').exists())
+        self.assertTrue(ScheduleMeet.objects.count(), 1)
 
-    def test_delete_meet(self):
-        meeting = ScheduleMeet.objects.create(
-            project=self.project,
-            title='Test meeting',
-            description='Test description',
-            meeting_date='2028-12-01',
-            start_time='12:00',
-            end_time='12:30',
-        )
         meeting.delete()
         self.assertEqual(ScheduleMeet.objects.count(), 0)
 
-    def test_create_message(self):
+    def test_message_board(self):
+        self.assertFalse(Thread.objects.filter(title='Test message').exists())
+        self.assertFalse(Message.objects.filter(title='Test reply').exists())
+
         message = Thread.objects.create(
             project=self.project,
             posted_by=self.user,
@@ -121,22 +105,9 @@ class Models(TestCase):
         self.assertEqual(reply.title, 'Test reply')
         self.assertEqual(reply.project, self.project)   
         self.assertEqual(reply.thread, message)
+        self.assertTrue(Thread.objects.filter(title='Test message').exists())
+        self.assertTrue(Message.objects.filter(title='Test reply').exists())
     
-    def test_delete_message(self):
-        message = Thread.objects.create(
-            project=self.project,
-            posted_by=self.user,
-            title='Test message',
-            body='Test body',
-            pinned=True
-        )
-
-        reply = Message.objects.create(
-            project=self.project,
-            title='Test reply',
-            posted_by=self.user,
-            thread=message
-        )
         reply.delete()
         self.assertEqual(Thread.objects.count(), 1)
         self.assertEqual(Message.objects.count(), 0)
@@ -145,6 +116,8 @@ class Models(TestCase):
         self.assertEqual(Message.objects.count(), 0)
 
     def test_file_upload(self):
+        self.assertFalse(FileUpload.objects.filter(file_title='Test file').exists())
+
         file = FileUpload.objects.create(
             project=self.project,
             uploaded_by=self.user,
@@ -153,32 +126,36 @@ class Models(TestCase):
             description='Test description',
             keywords='test, file, upload'
         )
+        self.assertTrue(FileUpload.objects.filter(file_title='Test file').exists())
         self.assertEqual(file.file_title, 'Test file')
         self.assertEqual(file.description, 'Test description')
         self.assertEqual(file.keywords, 'test, file, upload')
 
-    def test_delete_file(self):
-        file = FileUpload.objects.create(
-            project=self.project,
-            uploaded_by=self.user,
-            file='uploads/testfile.txt',
-            file_title='Test file',
-            description='Test description',
-            keywords='test, file, upload'
-        )
         file.delete()
+        self.assertFalse(FileUpload.objects.filter(file_title='Test file').exists())
         self.assertEqual(FileUpload.objects.count(), 0)
     
     def test_collaboration(self):
+        self.assertFalse(Collaboration.objects.filter(title='Test collaboration').exists())
+
         collaboration = Collaboration.objects.create(
             project=self.project,
             title='Test collaboration',
             description='Test description',
             created_by=self.user
         )
+        self.assertTrue(Collaboration.objects.filter(title='Test collaboration').exists())
         self.assertEqual(collaboration.title, 'Test collaboration')
         self.assertEqual(collaboration.description, 'Test description')
         self.assertEqual(collaboration.created_by, self.user)
+
+    def test_leave_project(self):
+        self.project.members.add(self.user)
+        self.assertEqual(self.project.members.count(), 1)
+        self.project.members.remove(self.user)
+        self.assertEqual(self.project.members.count(), 0)
+        self.project.delete()
+        self.assertFalse(Project.objects.filter(name='Test Project').exists())
 
 class Views(TestCase):
     def setUp(self):
@@ -193,7 +170,6 @@ class Views(TestCase):
         response = self.client.get(reverse('projects:project-detail', args=[self.project.id]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'project_detail.html')
-        self.assertTemplateUsed(response, 'base.html')
 
     def test_create_project_view(self):
         response = self.client.get(reverse('projects:create-project'))
@@ -205,3 +181,30 @@ class Views(TestCase):
         response = self.client.get(reverse('projects:calendar', args=[self.project.id]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'specific_pages/calendar.html')
+
+    def test_collaboration_view(self):
+        response = self.client.get(reverse('projects:collaboration', args=[self.project.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'specific_pages/collaboration.html')
+    
+    def test_todo_view(self):
+        response = self.client.get(reverse('projects:todos', args=[self.project.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'specific_pages/todos.html')
+
+    def test_message_board_view(self):
+        response = self.client.get(reverse('projects:message_board', args=[self.project.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'specific_pages/message_board.html')
+    
+    def test_schedule_meets_view(self):
+        response = self.client.get(reverse('projects:schedule-meets', args=[self.project.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'specific_pages/schedule_meets.html')
+    
+    def test_files_view(self):
+        response = self.client.get(reverse('projects:project_files', args=[self.project.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'specific_pages/files.html')
+
+    
