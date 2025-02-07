87c4b266928841896e0f82fb13e91415e448f891
tinavu <tinanvu04@gmail.com>
2025-02-06 19:15:56 -0500

    lol

--- AcademicProjectTracker/settings.py
+++ AcademicProjectTracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '84o1u(0-f91czupvyn+8^le2h%=o+!-7j&3+)-%^v!nmgo=ap1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
